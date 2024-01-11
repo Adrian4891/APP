01abbd7490bb09014c71ee0ab329e5978b246b6f
Adrian4891 <ernebarra36@gmail.com>
2024-01-11 18:41:26 -0300

    migrando deploy

--- server/src/index.js
+++ server/src/index.js
@@ -3,8 +3,8 @@ require("dotenv").config();
 const { PORT } = process.env;
 const { dataBase } = require("./dbConexion");
 
-dataBase.sync({force: false}).then(()=>{
+//dataBase.sync({force: false}).then(()=>{
     server.listen(PORT, ()=>{
        console.log(`server in port ${PORT}`)
     });
-})
+//})
