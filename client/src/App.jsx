85a813ff1f5a0b164bc50d9496c06ec275ad97a5
Adrian4891 <ernebarra36@gmail.com>
2024-01-15 18:04:44 -0300

    uri

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -30,7 +30,6 @@ import { useSelector, useDispatch } from "react-redux";
 import { getUserRoll } from './redux/actions';
 import  axios from "axios";
 axios.defaults.baseURL='https://barekintrumentsapp-production.up.railway.app/';
-
 function App() {
 
   const [categories, setCategories] = useState([]);
