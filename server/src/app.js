af1808a3c0522f951b2b3824766ea3d1f47f2d5b
Adrian4891 <ernebarra36@gmail.com>
2024-01-15 12:40:10 -0300

    url

--- server/src/app.js
+++ server/src/app.js
@@ -14,7 +14,6 @@ const routerNotifications = require("./routes/notificationsRouter");
 
 server.use(express.json());
 server.use(express.urlencoded({ extended : true }));
-//https://barek-intruments-app.vercel.app/
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://barek-intruments-app.vercel.app/');
   res.header('Access-Control-Allow-Credentials', 'true');
