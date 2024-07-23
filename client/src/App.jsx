77fffd7910974b109f80fbc8195b99ac7356293b
Adrian4891 <ernebarra36@gmail.com>
2024-07-23 15:33:53 -0300

    agregando config vercel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -29,7 +29,7 @@ import { useLocation } from 'react-router-dom';
 import { useSelector, useDispatch } from "react-redux";
 import { getUserRoll } from './redux/actions';
 import  axios from "axios";
-axios.defaults.baseURL='https://barekappserver.onrender.com';
+axios.defaults.baseURL='http://localhost:3001';
 
 function App() {
 
